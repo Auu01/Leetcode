b1059aa1518b91f49716a4285dff49445bde03ed
Run Guo <146129385+Auu01@users.noreply.github.com>
2024-04-12 20:45:52 -0400

    Update Q19 by using double pointers
    Update Q19 by using double pointers

--- Q19 Remove Nth Node From End of List.py
+++ Q19 Remove Nth Node From End of List.py
@@ -5,7 +5,8 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-
+        # Solution1 Two loops
+        '''
         if (head.next == None) and (n == 1):
             head = None
             return head
@@ -17,7 +18,7 @@ class Solution:
             while node.next != None:
                 count += 1
                 node = node.next
-            remove = count - n  # 3
+            remove = count - n #3
             if count == n:
                 return head.next
             while point.next != None:
@@ -27,3 +28,19 @@ class Solution:
                     break
                 point = point.next
             return head
+        '''
+        # Solution2: Double pointer
+        first = head
+        last = head
+        for i in range(n):
+            last = last.next
+        # 如果n等于链表长度，则last.next等于None，此时需要删除的是链表的第一个元素
+        if last == None:
+            return head.next
+        while last.next != None:
+            first = first.next
+            last = last.next
+        first.next = first.next.next
+        return head
+
+
