d3598d7837fd0b8392e332bc2cd8423ac626e218
Run Guo <146129385+Auu01@users.noreply.github.com>
2024-09-09 23:24:09 -0400

    Create Q145. Binary tree postorder traversal.py
    Done

---
+++ Q145. Binary tree postorder traversal.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        #后序遍历：左右根
+        if root:
+            result.extend(self.postorderTraversal(root.left))
+            result.extend(self.postorderTraversal(root.right))
+            result.append(root.val)
+        return result
